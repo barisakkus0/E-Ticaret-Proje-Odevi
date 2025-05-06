5909dc0242c37afa224d287a65aad3179f95d260
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-05-07 00:49:31 +0300

    Added Controllers

---
+++ Eticaretproject/Controllers/KullanicilarController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Eticaretproject.Models;
+using System.Linq;
+
+namespace Eticaretproject.Controllers
+{
+    public class KullanicilarController : Controller
+    {
+        private readonly EticaretContext _context;
+
+        public KullanicilarController(EticaretContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var kullanicilar = _context.Kullanicilars.ToList();
+            return View(kullanicilar);
+        }
+    }
+}
---
+++ Eticaretproject/Controllers/SiparislersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Eticaretproject.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore; // Kullanıcı bilgisini eager loading ile almak için
+
+namespace Eticaretproject.Controllers
+{
+    public class SiparislersController : Controller
+    {
+        private readonly EticaretContext _context;
+
+        public SiparislersController(EticaretContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Siparişleri kullanıcı bilgileriyle birlikte getiriyoruz
+            var siparisler = _context.Siparislers.Include(s => s.Kullanici).ToList();
+            return View(siparisler);
+        }
+    }
+}
