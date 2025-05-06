218d14fe379bcbd1abd27c465a3075750a442ec9
İsa Acar <isacar7221@gmail.com>
2025-05-06 23:44:44 +0300

    Frontend Kısmı İle Alakalı Eklemeler Yapıldı
    Sepet, Log in, Sign Up

---
+++ Eticaretproject/Controllers/KullanicilarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eticaretproject.Controllers
+{
+    public class KullanicilarController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Eticaretproject/Controllers/SepetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eticaretproject.Controllers
+{
+    public class SepetController : Controller
+    {
+        public IActionResult Sepet()
+        {
+            return View();
+        }
+    }
+}
